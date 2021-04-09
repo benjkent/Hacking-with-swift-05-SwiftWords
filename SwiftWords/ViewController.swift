211ea33b0b4322d76e29ff72b4d4ecdecec63094
Benjamin Kent <benjaminfranklinkent@frontier.com>
2021-04-09 13:47:34 -0700

    Initial Commit

---
+++ SwiftWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftWords
+//
+//  Created by benjamin kent on 4/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
